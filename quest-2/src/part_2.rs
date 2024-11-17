4ff670b1ee60443cbbc3050949ec745625fc3ad4
Mikkel <Mikkel123sej@gmail.com>
2024-11-18 00:56:50 +0100

    finished q2p2, work on q2p3

--- quest-2/src/part_2.rs
+++ quest-2/src/part_2.rs
@@ -1,36 +1,59 @@
 use std::collections::HashSet;
 
 #[allow(unused_imports)]
-use winnow::{
-    ascii::*, combinator::*, prelude::*, stream::AsChar,
-    token::*,
-};
+use winnow::{ascii::*, combinator::*, prelude::*, stream::AsChar, token::*};
 
 const INPUT_P2: &str = include_str!("../../input/everybody_codes_e2024_q02_p2.txt");
 /// note: new thing is just the old thing but with unique symbols
 fn main() {
-    println!("Result: {}", parse_input(INPUT_P2).unwrap());
+    println!(
+        "Result: {}",
+        parse_input(INPUT_P2).expect("could not parse input")
+    );
 }
 
 fn parse_input(mut s: &str) -> PResult<u32> {
     let (runes, inscription) = parse_parts.parse_next(&mut s)?;
+    // note: inscription is a str of all the lines, no separation is explicit
 
-    todo!()
+    let mut total = 0;
+    let mut frontier = 0;
+
+    for i in 0..inscription.len() {
+        for rune in runes.iter() {
+            let rune_len = rune.len();
+            if let Some(next_word) = inscription.get(i..(i + rune_len)) {
+                if next_word == *rune || next_word.chars().rev().eq(rune.chars()) {
+                    let offset = rune_len;
+                    let next_frontier = frontier.max(i + offset);
+                    let points = rune_len.min(next_frontier - frontier) as u32;
+                    total += points;
+                    #[cfg(debug_assertions)]
+                    {
+                        eprintln!("RUNE MATCH: {}", next_word);
+                        eprintln!("- FRONTIER: {}", frontier);
+                        eprintln!("- NEXT:     {} (i + {})", next_frontier, offset);
+                        eprintln!("- POINTS:   {} (total: {})", points, total);
+                        eprintln!();
+                    }
+                    frontier = next_frontier;
+                }
+            }
+        }
+    }
+
+    Ok(total)
 }
 
-fn parse_parts<'s>(input: &mut &'s str) -> PResult<(HashSet<&'s str>, Vec<&'s str>)> {
-    separated_pair(parse_top, multispace1, parse_inscription).parse_next(input)
+fn parse_parts<'s>(input: &mut &'s str) -> PResult<(HashSet<&'s str>, &'s str)> {
+    separated_pair(parse_top, (line_ending, line_ending), rest).parse_next(input)
 }
-
 fn parse_top<'s>(input: &mut &'s str) -> PResult<HashSet<&'s str>> {
-    preceded("WORDS:", parse_runes).parse_next(input)
+    let mut top = take_until(0.., "\n").parse_next(input)?;
+    preceded("WORDS:", parse_runes).parse_next(&mut top)
 }
 fn parse_runes<'s>(input: &mut &'s str) -> PResult<HashSet<&'s str>> {
-    separated(0.., rest, ",").parse_next(input)
-}
-
-fn parse_inscription<'s>(input: &mut &'s str) -> PResult<Vec<&'s str>> {
-    separated(0.., rest, multispace1).parse_next(input)
+    separated(1.., alpha1, ',').parse_next(input)
 }
 
 #[cfg(test)]
@@ -49,10 +72,9 @@ POWE PO WER P OWE R
 THERE IS THE END"
         };
         assert_eq!(
-            parse_input(SAMPLE).unwrap(),
+            parse_input(SAMPLE).expect("should be able to parse this"),
             37,
-            "There should be 37 runic symbols in total on \
-        inscription"
+            "There should be 37 runic symbols in total on inscription"
         );
     }
 }
--- quest-2/src/part_3.rs
+++ quest-2/src/part_3.rs
@@ -1,3 +1,67 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+#[allow(unused_imports)]
+use winnow::{ascii::*, combinator::*, prelude::*, stream::AsChar, token::*};
+
+const INPUT_P3: &str = include_str!("../../input/everybody_codes_e2024_q02_p3.txt");
 fn main() {
-    todo!()
+    println!(
+        "Result: {}",
+        parse_input(INPUT_P3).expect("could not parse")
+    );
+}
+
+fn parse_input(mut s: &str) -> PResult<u32> {
+    let (runes, armour) =
+        separated_pair(parse_top, (line_ending, line_ending), parse_bottom).parse_next(&mut s)?;
+    Ok(armour.all_runes_scale_locations(&runes))
+}
+
+fn parse_top<'s>(input: &mut &'s str) -> PResult<HashSet<&'s str>> {
+    preceded("WORDS:", parse_runes).parse_next(input)
+}
+fn parse_runes<'s>(input: &mut &'s str) -> PResult<HashSet<&'s str>> {
+    separated(0.., alpha1, ',').parse_next(input)
+}
+
+fn parse_bottom<'s>(input: &mut &'s str) -> PResult<ScaleArmour> {
+    todo!("Need to figure out how to parse and build a scale armour representation")
+}
+
+/// Some sort of scale location
+type Loc = usize;
+type Scale = char;
+struct ScaleArmour(Vec<Scale>);
+impl ScaleArmour {
+    fn all_runes_scale_locations(&self, runes: &HashSet<&str>) -> u32 {
+        runes
+            .iter()
+            .flat_map(|rune| self.rune_scale_locations(rune))
+            .unique()
+            .count() as u32
+    }
+    fn rune_scale_locations(&self, s: &str) -> HashSet<Loc> {
+        todo!("Look through all directions for matches, then create set of them")
+    }
+}
+
+#[cfg(test)]
+mod q2p3 {
+    use crate::parse_input;
+
+    #[test]
+    fn sample_works() {
+        //noinspection SpellCheckingInspection
+        const SAMPLE: &str = {
+            "WORDS:THE,OWE,MES,ROD,RODEO
+
+HELWORLT
+ENIGWDXL
+TRODEOAL"
+        };
+        assert_eq!(
+            parse_input(SAMPLE).expect("should be able to parse this"),
+            10
+        )
+    }
 }
